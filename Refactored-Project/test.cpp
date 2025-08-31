7c0e2cb6f71d7cd61295444c38b69b33ed541e01
xhxkhxysysjvkv <182754633a@gmail.com>
2025-08-31 23:03:37 +0800

    remove  Remote-Expert-Support-System-refactored

--- Refactored-Project/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include <QTimer>
-#include <QDebug>
-#include "test/conference_test.h"
-
-// int main(int argc, char *argv[])
-// {
-//     conferenceTest(argc, argv);
-// }
--- Remote-Expert-Support-System-refactored/README.md
+++
@@ -1,1 +0,0 @@
-# Remote-Expert-Support-System
