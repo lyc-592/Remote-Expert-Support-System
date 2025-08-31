c54c83bba0953609d49eada717a1bb36944aaa3c
Legna-lil <2642569199@qq.com>
2025-08-31 14:49:56 +0800

    Delete Remote-Expert-Support-System directory
    乱套了 淦

--- Remote-Expert-Support-System/utils/network_interface_constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef NETWORK_INTERFACE_CONSTANTS_H
-#define NETWORK_INTERFACE_CONSTANTS_H
-
-// 网络接口测试常数
-static const QString host = "127.0.0.1";
-static const quint16 hostPort = 1234;
-
-#endif // NETWORK_INTERFACE_CONSTANTS_H
